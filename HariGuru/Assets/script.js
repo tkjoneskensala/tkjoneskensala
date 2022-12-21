e77f99cee95f3a2aece2301d6dfa15cde0e48413
tkjoneskensala <tkj1skensala@gmail.com>
2022-12-21 19:39:49 +0700

    testing scrolling

---
+++ HariGuru/Assets/script.js
@@ -0,0 +1,25 @@
+const showMoreButton = document.getElementById("showMore");
+const showMoreItems = document.querySelectorAll(".itemLebih");
+let showItemMax = 0;
+
+const showMore = n => {
+    let o = null;
+    for (let i=showItemMax; i<n; i++) {
+        showMoreItems[i].classList.remove("hidden");
+    };
+    (n == 0) || (n == 3) ? o = 1 : o = 2;
+    showMoreButton.setAttribute("onclick", `showMore(${n+o})`);
+    showItemMax = n;
+    if (showItemMax == showMoreItems.length) {
+        showMoreButton.innerText = "Tutup";
+        showMoreButton.setAttribute("onclick", "closedMore()")
+    };
+};
+
+const closedMore = () => {
+    for (let i=0; i<showMoreItems.length; i++) {
+        showMoreItems[i].classList.add("hidden");
+    };
+    showMoreButton.innerText = "Selengkapnya";
+    window.scroll(0, window.scrollY - 100);
+};
